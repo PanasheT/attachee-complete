1c2bf456aff7c297cdeee201c1a480dbd1f55e6b
PanasheT <ptafuma@proton.me>
2023-02-25 19:27:10 +0200

    feat: add create daily log endpoint

--- src/modules/daily-log/controllers/daily-log.controller.ts
+++ src/modules/daily-log/controllers/daily-log.controller.ts
@@ -1,9 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateDailyLogDto, DailyLogDto, DailyLogDtoFactory } from '../dtos';
+import { DailyLogEntity } from '../entities';
 import { DailyLogService } from '../services';
 
 @Controller('daily-logs')
 @ApiTags('daily-logs')
 export class DailyLogController {
   constructor(private readonly service: DailyLogService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a daily log.' })
+  @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({
+    description: 'Daily log successfully created.',
+    type: DailyLogDto,
+  })
+  public async createDailyLog(
+    @Body() model: CreateDailyLogDto
+  ): Promise<DailyLogDto> {
+    const dailyLog: DailyLogEntity = await this.service.createDailyLog(model);
+    return DailyLogDtoFactory(dailyLog);
+  }
 }
