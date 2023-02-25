db9407960163d6a4129e32cca5ed6c35b90e5c36
PanasheT <ptafuma@proton.me>
2023-02-25 15:18:59 +0200

    feat: add create student to factory

--- src/modules/student/factories/student.factory.ts
+++ src/modules/student/factories/student.factory.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateStudentDto } from '../dtos';
 import { StudentEntity } from '../entities';
 
 @Injectable()
@@ -9,4 +10,26 @@ export class StudentFactory {
     @InjectRepository(StudentEntity)
     private readonly repo: Repository<StudentEntity>
   ) {}
+
+  public async createStudent(model: CreateStudentDto): Promise<StudentEntity> {
+    await this.assertStudentExists(model);
+    return Object.assign(new StudentEntity(), model);
+  }
+
+  private async assertStudentExists({
+    regNumber,
+    email,
+    phone,
+  }: CreateStudentDto): Promise<void> {
+    const deleted: boolean = false;
+    const query = [
+      { regNumber, deleted },
+      { email, deleted },
+      { phone, deleted },
+    ];
+
+    if (await this.repo.findOneBy(query)) {
+      throw new BadRequestException('Student already exists.');
+    }
+  }
 }
--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentController } from './controllers';
 import { StudentEntity } from './entities';
+import { StudentFactory } from './factories';
 import { StudentService } from './services';
 
 @Module({
   imports: [TypeOrmModule.forFeature([StudentEntity])],
   controllers: [StudentController],
   exports: [StudentService],
-  providers: [StudentService],
+  providers: [StudentService, StudentFactory],
 })
 export class StudentModule {}
