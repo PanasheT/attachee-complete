fbdd8b0fe4bab514994eac8263b2871b68ec18e2
PanasheT <ptafuma@proton.me>
2023-02-25 15:14:26 +0200

    fix: make dto properties optional

--- src/modules/student/dtos/update-student.dto.ts
+++ src/modules/student/dtos/update-student.dto.ts
@@ -1,9 +1,11 @@
-import { PickType } from '@nestjs/swagger';
+import { PartialType, PickType } from '@nestjs/swagger';
 import { CreateStudentDto } from './create-student.dto';
 
-export class UpdateStudentDto extends PickType(CreateStudentDto, [
-  'email',
-  'phone',
-  'programmeCode',
-  'yearOfStudy',
-] as const) {}
+export class UpdateStudentDto extends PartialType(
+  PickType(CreateStudentDto, [
+    'email',
+    'phone',
+    'programmeCode',
+    'yearOfStudy',
+  ] as const)
+) {}
