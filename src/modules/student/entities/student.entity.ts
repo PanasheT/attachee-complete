68a809f26f491af7bbfdd1cbdfb8f4bb833da6c2
PanasheT <ptafuma@proton.me>
2023-02-25 15:06:55 +0200

    feat: define student entity

--- src/modules/student/entities/student.entity.ts
+++ src/modules/student/entities/student.entity.ts
@@ -1,5 +1,32 @@
 import { AbstractEntity } from 'src/common';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'student' })
-export class StudentEntity extends AbstractEntity {}
+export class StudentEntity extends AbstractEntity {
+  @Column({ unique: true, update: false })
+  regNumber: string;
+
+  @Column({ type: 'smallint', default: 3 })
+  yearOfStudy: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  courseCode: string;
+
+  @Column()
+  university: string;
+
+  @Column()
+  password: string;
+}
