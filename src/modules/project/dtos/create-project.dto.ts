5d369b5f4966be0587e15f4528f72679557ba723
PanasheT <ptafuma@proton.me>
2023-02-25 20:14:25 +0200

    feat: define create project dto

--- src/modules/project/dtos/create-project.dto.ts
+++ src/modules/project/dtos/create-project.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProjectDto {}
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+import { ProjectStatus } from '../entities';
+
+export class CreateProjectDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly description: string;
+
+  @IsEnum(ProjectStatus)
+  @IsNotEmpty()
+  readonly status: ProjectStatus;
+
+  @IsDate()
+  @IsOptional()
+  readonly startDate: Date;
+
+  @IsDate()
+  @IsOptional()
+  readonly endDate: Date;
+
+  @IsDate()
+  @IsOptional()
+  readonly estimatedEndDate: Date;
+
+  @IsUrl()
+  @IsOptional()
+  readonly gitRepoUrl: string;
+}
