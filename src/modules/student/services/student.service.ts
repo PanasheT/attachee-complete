1397b6515b2231011b4a5ee3e0d1da2b557704e4
PanasheT <ptafuma@proton.me>
2023-02-25 15:41:08 +0200

    feat: add find student methods

--- src/modules/student/services/student.service.ts
+++ src/modules/student/services/student.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { StudentEntity } from '../entities';
+import {
+  FindStudentQuery,
+  StudentIdentificationProperties,
+} from '../types/student.types';
 
 @Injectable()
 export class StudentService {
@@ -9,4 +13,39 @@ export class StudentService {
     @InjectRepository(StudentEntity)
     private readonly repo: Repository<StudentEntity>
   ) {}
+
+  public async findOneStudent(
+    value: string,
+    property: StudentIdentificationProperties
+  ): Promise<StudentEntity> {
+    const query: FindStudentQuery = this.generateStudentQuery(value, property);
+    return query ? await this.repo.findOneBy(query) : undefined;
+  }
+
+  public async findOneStudentOrFail(
+    value: string,
+    property: StudentIdentificationProperties
+  ): Promise<StudentEntity> {
+    try {
+      const query: FindStudentQuery = this.generateStudentQuery(
+        value,
+        property
+      );
+      return query ? await this.repo.findOneByOrFail(query) : undefined;
+    } catch {
+      throw new NotFoundException('Student not found.');
+    }
+  }
+
+  public async findAllStudents(): Promise<StudentEntity[]> {
+    return await this.repo.findBy({ deleted: false });
+  }
+
+  private generateStudentQuery(
+    value: string,
+    property: StudentIdentificationProperties,
+    deleted: boolean = false
+  ): FindStudentQuery {
+    return { [property]: value, deleted };
+  }
 }
