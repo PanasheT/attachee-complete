0968ae6675da0b0b3b9f0576efad7c8cfb4b6148
PanasheT <ptafuma@proton.me>
2023-02-25 19:11:36 +0200

    feat(): add create daily-log dto

--- src/modules/daily-log/dtos/create-daily-log.dto.ts
+++ src/modules/daily-log/dtos/create-daily-log.dto.ts
@@ -1,1 +1,33 @@
-export class CreateDailyLogDto {}
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateDailyLogDto {
+  @IsUUID()
+  @IsNotEmpty()
+  readonly studentUUID: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly description: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  readonly checkIn: Date;
+
+  @IsDate()
+  @IsNotEmpty()
+  readonly checkOut: Date;
+
+  @IsString()
+  @IsOptional()
+  readonly difficulties?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly comment?: string;
+}
