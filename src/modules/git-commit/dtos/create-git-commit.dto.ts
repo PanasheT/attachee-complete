077c2264f9dba2323c2a6684bded93bba71911a9
PanasheT <ptafuma@proton.me>
2023-02-26 10:01:25 +0200

    feat: define create git commit dto

--- src/modules/git-commit/dtos/create-git-commit.dto.ts
+++ src/modules/git-commit/dtos/create-git-commit.dto.ts
@@ -1,1 +1,15 @@
-export class CreateGitCommitDto {}
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class CreateGitCommitDto {
+  @IsUUID()
+  @IsNotEmpty()
+  readonly dailyLogUUID: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly commitHash: string;
+
+  @IsString()
+  @IsOptional()
+  readonly commitMessage: string;
+}
