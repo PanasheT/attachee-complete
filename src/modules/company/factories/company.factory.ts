f4300d874df50a7671318830fd16ab39c23b39ac
PanasheT <ptafuma@proton.me>
2023-02-26 11:41:39 +0200

    feat: add create company method to factory

--- src/modules/company/factories/company.factory.ts
+++ src/modules/company/factories/company.factory.ts
@@ -1,13 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCompanyDto } from '../dtos';
 import { CompanyEntity } from '../entities';
-
 
 @Injectable()
 export class CompanyFactory {
-    constructor(
-        @InjectRepository(CompanyEntity)
-        private readonly repo: Repository<CompanyEntity>
-    ) {}
+  constructor(
+    @InjectRepository(CompanyEntity)
+    private readonly repo: Repository<CompanyEntity>
+  ) {}
+
+  public async createCompany(model: CreateCompanyDto): Promise<CompanyEntity> {
+    await this.assertCompanyExists(model.name);
+    return Object.assign(new CompanyEntity(), model);
+  }
+
+  private async assertCompanyExists(name: string): Promise<void> {
+    const company: CompanyEntity = await this.repo.findOneBy({
+      name,
+      deleted: false,
+    });
+
+    if (company) {
+      throw new NotAcceptableException('Company already exists');
+    }
+  }
 }
