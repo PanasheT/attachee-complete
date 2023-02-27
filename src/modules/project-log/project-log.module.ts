469a1fb834d1421d181f168c786a99d5ccc40a98
PanasheT <ptafuma@proton.me>
2023-02-27 09:34:13 +0200

    feat: add find create method to project log service

--- src/modules/project-log/project-log.module.ts
+++ src/modules/project-log/project-log.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectModule } from '../project/project.module';
 import { ProjectLogController } from './controllers';
 import { ProjectLogEntity } from './entities';
 import { ProjectLogFactory } from './factories';
 import { ProjectLogService } from './services';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProjectLogEntity]), ProjectLogModule],
+  imports: [TypeOrmModule.forFeature([ProjectLogEntity]), ProjectModule],
   controllers: [ProjectLogController],
   exports: [ProjectLogService],
   providers: [ProjectLogService, ProjectLogFactory],
--- src/modules/project-log/services/project-log.service.ts
+++ src/modules/project-log/services/project-log.service.ts
@@ -1,13 +1,27 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ProjectEntity } from 'src/modules/project/entities';
+import { ProjectService } from 'src/modules/project/services';
 import { Repository } from 'typeorm';
+import { CreateProjectLogDto } from '../dtos';
 import { ProjectLogEntity } from '../entities';
+import { ProjectLogFactory } from '../factories';
 
 @Injectable()
 export class ProjectLogService {
+  private logger = new Logger(ProjectLogService.name);
+
   constructor(
     @InjectRepository(ProjectLogEntity)
-    private readonly repo: Repository<ProjectLogEntity>
+    private readonly repo: Repository<ProjectLogEntity>,
+    private readonly factory: ProjectLogFactory,
+    private readonly projectService: ProjectService
   ) {}
 
   public async findOneProjectLog(uuid: string): Promise<ProjectLogEntity> {
@@ -34,6 +48,34 @@ export class ProjectLogService {
       return await this.repo.findOneByOrFail({ uuid, deleted: false });
     } catch {
       throw new NotFoundException('Project log not found.');
+    }
+  }
+
+  public async createProjectLog(
+    model: CreateProjectLogDto
+  ): Promise<ProjectLogEntity> {
+    return;
+  }
+
+  public getProjectLogFromFactory(
+    model: Omit<CreateProjectLogDto, 'projectUUID'>,
+    project: ProjectEntity
+  ): ProjectLogEntity {
+    try {
+      return this.factory.createProjectLog(model, project);
+    } catch (error) {
+      throw new HttpException(error?.message, error?.status);
+    }
+  }
+
+  public async handleProjectLogSave(
+    model: ProjectLogEntity
+  ): Promise<ProjectLogEntity> {
+    try {
+      return await this.repo.save(model);
+    } catch (error) {
+      this.logger.error(error?.message || error);
+      throw new InternalServerErrorException('Failed to save project log.');
     }
   }
 }
