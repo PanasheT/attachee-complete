465b62c449440fdc9aa50b81e7b52efbca98666d
PanasheT <ptafuma@proton.me>
2023-02-27 12:13:43 +0200

    feat: add student login route

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,9 +1,23 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { StudentLoginDto, StudentLoginResultDto } from '../dtos';
 import { AuthService } from '../services';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly service: AuthService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Login a student.' })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Student successfully logged in.',
+    type: StudentLoginResultDto,
+  })
+  public async loginStudent(
+    @Body() model: StudentLoginDto
+  ): Promise<StudentLoginResultDto> {
+    return this.service.loginStudent(model);
+  }
 }
