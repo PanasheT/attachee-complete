3b8bcb83ddf70023ee7c858869ba84b39e9bc872
PanasheT <ptafuma@proton.me>
2023-03-04 14:49:07 +0200

    feat: define create supervisor dto

--- src/modules/supervisor/dtos/create-supervisor.dto.ts
+++ src/modules/supervisor/dtos/create-supervisor.dto.ts
@@ -1,1 +1,23 @@
-export class CreateSupervisorDto {}
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreateSupervisorDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+}
