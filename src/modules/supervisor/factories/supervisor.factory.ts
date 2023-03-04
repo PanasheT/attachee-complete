13e73ed761e3e695dca68cd55d8af54835ff6c71
PanasheT <ptafuma@proton.me>
2023-03-04 16:32:35 +0200

    feat: add update supervisor method to factory

--- src/modules/supervisor/factories/supervisor.factory.ts
+++ src/modules/supervisor/factories/supervisor.factory.ts
@@ -1,7 +1,8 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { validateUpdate } from 'src/util';
 import { Repository } from 'typeorm';
-import { CreateSupervisorDto } from '../dtos';
+import { CreateSupervisorDto, UpdateSupervisorDto } from '../dtos';
 import { SupervisorEntity } from '../entities';
 import { FindSupervisorQuery } from '../types';
 
@@ -15,23 +16,43 @@ export class SupervisorFactory {
   public async createSupervisor(
     model: CreateSupervisorDto
   ): Promise<SupervisorEntity> {
-    await this.assertSupervisorExists(model.email, model.phone);
+    await this.assertSupervisorExists({
+      email: model.email,
+      phone: model.phone,
+    });
 
     return Object.assign(new SupervisorEntity(), { model });
   }
 
-  private async assertSupervisorExists(
-    email: string,
-    phone: string
-  ): Promise<void> {
+  private async assertSupervisorExists(model: {
+    email?: string;
+    phone?: string;
+  }): Promise<void> {
     const deleted: boolean = false;
     const query: FindSupervisorQuery[] = [
-      { email, deleted },
-      { phone, deleted },
-    ];
+      model.email && { email: model.email, deleted },
+      model.phone && { phone: model.phone, deleted },
+    ].filter(Boolean);
 
-    if (Boolean(await this.repo.findOneBy(query))) {
+    if (query.length > 0 && Boolean(await this.repo.findOneBy(query))) {
       throw new BadRequestException('Supervisor already exists.');
     }
+  }
+
+  public async updateSupervisor(
+    model: UpdateSupervisorDto,
+    supervisor: SupervisorEntity
+  ): Promise<SupervisorEntity> {
+    const validatedDto: Partial<SupervisorEntity> = validateUpdate(
+      supervisor,
+      model
+    );
+
+    await this.assertSupervisorExists({
+      email: validatedDto?.email,
+      phone: validatedDto?.phone,
+    });
+
+    return Object.assign(supervisor, { model });
   }
 }
