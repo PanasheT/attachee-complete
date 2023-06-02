229c278f4da10ef84f235fa98c556dd9773c8e39
PanasheT <ptafuma@proton.me>
2023-06-02 13:56:33 +0200

    feat(task): define task entity

--- src/modules/task/entities/task.entity.ts
+++ src/modules/task/entities/task.entity.ts
@@ -1,5 +1,41 @@
 import { AbstractEntity } from 'src/common';
-import { Entity } from 'typeorm';
+import { TaskStatus } from 'src/modules/project-log/entities';
+import { StudentEntity } from 'src/modules/student/entities';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { SupervisorEntity } from './../../supervisor/entities/supervisor.entity';
+
+export enum PriorityEnum {
+  LOW = 'LOW',
+  MEDIUM = 'MEDIUM',
+  HIGH = 'HIGH',
+  URGENT = 'URGENT',
+}
 
 @Entity({ name: 'task' })
-export class TaskEntity extends AbstractEntity {}
+export class TaskEntity extends AbstractEntity {
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ enum: TaskStatus })
+  status: TaskStatus;
+
+  @Column()
+  dueDate: Date;
+
+  @Column({ enum: PriorityEnum })
+  priority: PriorityEnum;
+
+  @Column({ default: null })
+  notes: string;
+
+  @ManyToOne(() => StudentEntity, { eager: true })
+  @JoinColumn()
+  student: StudentEntity;
+
+  @ManyToOne(() => SupervisorEntity, { eager: true })
+  @JoinColumn()
+  supervisor: SupervisorEntity;
+}
