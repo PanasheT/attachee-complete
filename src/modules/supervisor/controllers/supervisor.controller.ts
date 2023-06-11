f7af67e989d486210b4c41598255fb108793f1e9
PanasheT <ptafuma@proton.me>
2023-06-11 21:01:49 +0200

    feat(supervisor): route for update

--- src/modules/supervisor/controllers/supervisor.controller.ts
+++ src/modules/supervisor/controllers/supervisor.controller.ts
@@ -1,9 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Patch,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { isUUID } from 'class-validator';
+import { SupervisorEntity } from 'src/modules/supervisor/entities';
+import {
+  SupervisorDto,
+  SupervisorDtoFactory,
+  UpdateSupervisorDto,
+} from '../dtos';
 import { SupervisorService } from '../services';
 
 @Controller('supervisors')
 @ApiTags('supervisors')
 export class SupervisorController {
   constructor(private readonly service: SupervisorService) {}
+
+  @Patch(':uuid')
+  @ApiOperation({ summary: 'Update a supervisor by uuid' })
+  public async updateSupervisor(
+    @Param('uuid') uuid: string,
+    @Body() model: UpdateSupervisorDto
+  ): Promise<SupervisorDto> {
+    if (!isUUID(uuid)) {
+      throw new BadRequestException('Invalid uuid');
+    }
+
+    const supervisor: SupervisorEntity = await this.service.updateSupervisor(
+      model,
+      uuid
+    );
+    return SupervisorDtoFactory(supervisor);
+  }
 }
